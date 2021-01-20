0c912063c2ba937f8bc5843e70518a07054fa72e
Eduardo Marques <edrdo@dcc.fc.up.pt>
2021-01-20 18:55:57 +0000

    OpenZeppelin contract & test

---
+++ OpenZeppelin/contracts/token/ERC721/IERC721.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.2;
+
+import "../../introspection/IERC165.sol";
+
+/**
+ * @title ERC721 Non-Fungible Token Standard basic interface
+ * @dev see https://eips.ethereum.org/EIPS/eip-721
+ */
+contract IERC721 is IERC165 {
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+
+    function balanceOf(address owner) public view returns (uint256 balance);
+    function ownerOf(uint256 tokenId) public view returns (address owner);
+
+    function approve(address to, uint256 tokenId) public;
+    function getApproved(uint256 tokenId) public view returns (address operator);
+
+    function setApprovalForAll(address operator, bool _approved) public;
+    function isApprovedForAll(address owner, address operator) public view returns (bool);
+
+    function transferFrom(address from, address to, uint256 tokenId) public;
+    function safeTransferFrom(address from, address to, uint256 tokenId) public;
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public;
+}
